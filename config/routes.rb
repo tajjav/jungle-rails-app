f99fce09460c0611e5138f3704ef19c61343985e
tajjav <taj.jav@hotmail.com>
2024-03-01 10:09:07 -0700

    create about controller and show, view left

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   root to: 'products#index'
 
+  
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
 
@@ -11,6 +12,8 @@ Rails.application.routes.draw do
   end
 
   resources :orders, only: [:create, :show]
+
+  get '/about' => 'about#show'
 
   namespace :admin do
     root to: 'dashboard#show'
