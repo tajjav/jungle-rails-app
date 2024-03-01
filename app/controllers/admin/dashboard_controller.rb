3fbdb86aa719b1bf31ae699f7400e22cbe32cc19
tajjav <taj.jav@hotmail.com>
2024-03-01 09:53:56 -0700

    admin show page categories cout and product count added

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,11 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: "Jungle", password: "book"
   def show
+    @categories_count = Category.count(:all)
+      # print categories_count
+    @products_count = Product.count(:all)
+    logger.error("categories count:#{@categories_count}")
+    #@category = Category.find(params[:id])
+    # @products = @category.products.order(created_at: :desc)
   end
 end
